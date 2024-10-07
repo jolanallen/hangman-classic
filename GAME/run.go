1addd11c233eca866a22dc45f06e6b73ad072b53
ajolan <jolan.allen@ynov.com>
2024-10-07 12:34:36 +0200

    game over and win

--- GAME/run.go
+++ GAME/run.go
@@ -90,7 +90,6 @@ func (hangman *HANGMAN) randomWord() {
 
 
 func (hangman *HANGMAN) win() {
-	for i := 0; i <= 20; i++ {
 		fmt.Println(`
  		__     __          __          ___       _ 
  		\ \   / /          \ \        / (_)     | |
@@ -103,7 +102,6 @@ func (hangman *HANGMAN) win() {
 
 		`)
 		fmt.Println("fin du jeu vous avez gagner le mot était bien", hangman.Mot)
-	}
 	hangman.IsRunning = false
 	
 	
@@ -111,7 +109,16 @@ func (hangman *HANGMAN) win() {
 }
 
 func (hangman *HANGMAN) gameOver() {
-	
+	fmt.Println(` 
+		   ____                          ___                 
+		  / ___| __ _ _ __ ___   ___    / _ \__   _____ _ __ 
+		 | |  _ / _' | '_ ' _ \ / _ \  | | | \ \ / / _ \ '__|
+		 | |_| | (_| | | | | | |  __/  | |_| |\ V /  __/ |
+	    	  \____|\__,_|_| |_| |_|\___|   \___/  \_/ \___|_|
+                                                                                              
+
+	`)
+
 	fmt.Print("fin du jeu le mot était : ")
 	fmt.Println(hangman.Mot)
 	hangman.IsRunning = false
